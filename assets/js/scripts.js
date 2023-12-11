82076170c1a781514e72827c6a8b5dd0543336a7
Melissa Wright <meliswright925@gmail.com>
2023-12-10 21:04:24 -0500

    added the questions and choices in an array

---
+++ assets/js/scripts.js
@@ -0,0 +1,166 @@
+
+// Quiz Questions and Choices
+var questions = [
+    {
+        question: "What does the acronym 'JSON' stand for?",
+        choices: [
+            "JavaScript Object Notation",
+            "JavaScript Output Network",
+            "Java Standard Object Notation",
+            "Java Systematic Output Namespace"
+        ],
+        correct: 0,
+    },
+    {
+        question: "How do you check the type of a variable in JavaScript?",
+        choices: [
+            "typeOf variable",
+            "varType(variable)",
+            "typeof variable",
+            "checkType(variable)"
+        ],
+        correct: 2,
+    },
+    {
+        question: "Which method is used to remove the last element from an array in JavaScript?",
+        choices: [
+            "array.pop()",
+            "array.removeLast()",
+            "array.deleteLast()",
+            "array.spliceLast()"
+        ],
+        correct: 0,
+    },
+    {
+        question: "What is the purpose of the 'event.preventDefault()' method in JavaScript?",
+        choices: [
+            "To stop the event propagation",
+            "To cancel the default action of an event",
+            "To force an event to occur",
+            "To log events to the console"
+        ],
+        correct: 1,
+    },
+    {
+        question: "Which operator is used for strict equality comparison in JavaScript?",
+        choices: [
+            "==",
+            "===",
+            "=",
+            "!="
+        ],
+        correct: 1,
+    },
+    {
+        question: "Inside of which HTML element do we store the JavaScript?",
+        choices: [
+        "<javaScript>",
+        "<scriptElement>",
+        "<js>",
+        "<script>"
+        ],
+        correct: 3,
+    },
+    {
+        question: "What is the correct JavaScript syntax to write 'Hello World'?",
+        choices: [ 
+            "response.write('Hello World')",
+            "'Hello World'",
+            "document.write('Hello World')",
+            "('Hello World')"
+        ],
+        correct: 2,
+    },
+    {
+        question: "What is the correct JavaScript syntax to change the content of the HTML Below?",
+        choices: [ 
+            "response.write('Hello World')",
+            "'Hello World'",
+            "document.write('Hello World')",
+            "('Hello World')"
+        ],
+        correct: 2,
+    },
+    {
+        question: "Where is the correct place to insert javaScript? ",
+        choices: [
+            "Any section works",
+            "The <body> section",
+            "The <head> section",
+            "The <footer> section"
+        ],
+        correct: 1,
+    },
+    {
+        question: "What is the syntax for an external script 'xyz.js' ",
+        choices: [
+            "<script src='xyz.js'",
+            "<script href='xyz.js'",
+            "<script name='xyx.js'",
+            "<script src:'xyx.js'"
+        ],
+        correct: 0,
+    },
+    {
+        question: "How do you create a function?",
+        choices: [
+            "function:myFunction()",
+            "function= myFunction()",
+            "myFunction() function",
+            "function myFunction()"
+        ],
+        correct: 3,
+    },
+    {
+
+        question: "Inside of which HTML element do we link an external stylesheet?",
+        choices: [
+            "<link rel='stylesheet' type='text/css' href='styles.css'>",
+            "<style src='external.css'>",
+            "<css href='external.css'>",
+            "<external-styles src='styles.css'>"
+        ],
+        correct: 0,
+    },
+    {
+        question: "What does the 'DOM' stand for in JavaScript?",
+        choices: [
+            "Document Object Model",
+            "Data Object Model",
+            "Dynamic Output Mechanism",
+            "Digital Object Memory"
+        ],
+        correct: 0,
+    },
+    {
+        question: "Which keyword is used to declare a variable in JavaScript?",
+        choices: [
+            "var",
+            "let",
+            "const",
+            "variable"
+        ],
+        correct: 0,
+    },
+    {
+        question: "What is the purpose of the 'console.log()' function in JavaScript?",
+        choices: [
+            "To print messages to the console",
+            "To display alerts",
+            "To create a new variable",
+            "To define a function"
+        ],
+        correct: 0,
+    },
+    {
+        question: "How do you comment a single line of code in JavaScript?",
+        choices: [
+            "// This is a comment",
+            "# This is a comment",
+            "/* This is a comment */",
+            "<!-- This is a comment -->"
+        ],
+        correct: 0,
+    }
+];
+
